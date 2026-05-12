a061f4741cce3e3ad9c0e86389a3bf4128135f06
Rajshah7 <63506258+Rajshah7@users.noreply.github.com>
2026-05-12 14:35:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(e) {
+	 e.preventDefault();
 
+	const firstName = document.querySelctor("input[name='fname']").value.trim();
+	const lastName = document.querySelector("input[name='lname']").value.trim();
+
+	alert(`${firstName} ${lastName}`)
 }
+
+document.getElementById('form1').addEventListener('submit',getFormvalue);
